267d7cd16cf5f20f1086cbc347266c2ec69ad213
MapesaSEI <oficinatecnica@mapesa.es>
2026-06-18 13:58:26 +0200

    añadir imagen roboteech

--- js/app.js
+++ js/app.js
@@ -56,8 +56,11 @@ const CATALOGO = [
         id: 'otramarca',
         nombre: 'Robotech',
         modelos: [
-            { id: 'om_m1', nombre: '3000 PGS', imagen: null, puntos: [
-                { id: 'p1',  nombre: 'Punto 1',  x: 50, y: 50 }
+            { id: 'om_m1', 
+            nombre: '3000 PGS', 
+            imagen: 'imagenes/robotech-3000pgs', 
+            puntos: [
+                { id: 'p1',  nombre: 'Punto 1',  x: 50, y: 50 },
             ] },
             { id: 'om_m2', nombre: 'Modelo 2', imagen: null, puntos: [] }
         ]
